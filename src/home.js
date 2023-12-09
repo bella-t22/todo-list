89f8556ccae16e89a306102ac066bb3b9b59912e
bella-t22 <b.jtorr22@gmail.com>
2023-12-09 15:52:22 -0800

    Basic project and task styling

--- src/home.js
+++ src/home.js
@@ -1,14 +1,8 @@
 import getCreateIcon from './icons/create.svg';
 
-const content = document.querySelector('#content');
-
-const header = document.createElement('div');
-header.classList.add('header');
-content.appendChild(header);
-
-const sidebar = document.createElement('div');
-sidebar.classList.add('sidebar');
-content.appendChild(sidebar);
+const header = document.querySelector('.header');
+const sidebar = document.querySelector('.sidebar');
+const main = document.querySelector('.main');
 
 const headerTitle = document.createElement('h1');
 headerTitle.textContent = 'To-do List';
@@ -37,6 +31,20 @@ const firstProject = document.createElement('p');
 firstProject.classList.add('project-names');
 firstProject.textContent = 'First Project';
 sidebar.appendChild(firstProject);
+
+const mainTitle = document.createElement('h1');
+mainTitle.classList.add('main-title');
+mainTitle.textContent = 'First Project';
+main.appendChild(mainTitle);
+
+const projectCard = document.createElement('div');
+projectCard.classList.add('project-card');
+main.appendChild(projectCard);
+
+const task = document.createElement('div');
+task.classList.add('task');
+task.textContent = 'First task';
+projectCard.appendChild(task);
 
 // create default project for todos to be added to
 // create project button to create new projects
--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
 import './app.css';
 import './home.js';
-
-console.log('hello from index');
