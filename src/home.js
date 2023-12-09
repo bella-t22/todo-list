f30e31ff93c5f838e956a40dc244468b50d8802a
bella-t22 <b.jtorr22@gmail.com>
2023-12-08 16:08:58 -0800

    Add content and styles to home.js

--- src/home.js
+++ src/home.js
@@ -1,3 +1,5 @@
+import getCreateIcon from './icons/create.svg';
+
 const content = document.querySelector('#content');
 
 const header = document.createElement('div');
@@ -15,6 +17,26 @@ header.appendChild(headerTitle);
 const sidebarTitle = document.createElement('h2');
 sidebarTitle.textContent = 'Projects';
 sidebar.appendChild(sidebarTitle);
+
+const addProjectDiv = document.createElement('div');
+addProjectDiv.classList.add('add-project-div');
+sidebar.appendChild(addProjectDiv);
+
+const createIcon = document.createElement('img');
+createIcon.classList.add('create-icon');
+// createIcon.src = getCreateIcon;
+console.log(getCreateIcon)
+addProjectDiv.appendChild(createIcon);
+
+const addProject = document.createElement('p');
+addProject.classList.add('add-project');
+addProject.textContent = 'Add Project';
+addProjectDiv.appendChild(addProject);
+
+const firstProject = document.createElement('p');
+firstProject.classList.add('project-names');
+firstProject.textContent = 'First Project';
+sidebar.appendChild(firstProject);
 
 // create default project for todos to be added to
 // create project button to create new projects
