1d49de89be383bdf00497c98c4823d1010c614ab
bella-t22 <b.jtorr22@gmail.com>
2023-12-27 16:57:41 -0800

    refactor createTask func for display

--- src/createTask.js
+++ src/createTask.js
@@ -1,11 +1,13 @@
 import {taskPopup} from './taskPopup.js';
 import {projects} from './project-data.js';
+import Task from './task-class.js';
 
 // add checkbox to check when task is done
 // task should not be added if inputs are empty
 // add an edit and a delete button to each task
-
-const addTaskBtn = document.querySelector('.add-task-btn');
+setTimeout(() => {
+    const addTaskBtn = document.querySelector('.add-task-btn');
+console.log(addTaskBtn)
 addTaskBtn.addEventListener('click', () => {
     const popup = document.querySelector('.task-popup');
     if (popup) {
@@ -13,14 +15,88 @@ addTaskBtn.addEventListener('click', () => {
     } else {
         taskPopup();
     }
-
     // button should not be allowed to be pressed if inputs are empty.
     const submitTask = document.querySelector('.submit-task');
     submitTask.addEventListener('click', createTask);
-});
+}), 500
+})
 
-function createTask() {
 
+// function createTask() {
+
+//     const projectCard = document.querySelector('.project-card');    
+//         const task = document.createElement('div');
+//         task.classList.add('task');
+//         projectCard.appendChild(task);
+
+//         const taskHeader = document.createElement('div');
+//         taskHeader.classList.add('task-header');
+//         task.appendChild(taskHeader);
+
+//         const taskTitle = document.createElement('h3');
+//         taskTitle.classList.add('task-title');
+//         const taskInput = document.querySelector('.task-input');
+//         taskTitle.textContent = taskInput.value;
+//         taskHeader.appendChild(taskTitle);
+
+//         const taskDueDiv = document.createElement('div');
+//         taskDueDiv.classList.add('task-due-div');
+//         taskHeader.appendChild(taskDueDiv);
+
+//         const taskBottomDiv = document.createElement('div');
+//         taskBottomDiv.classList.add('task-bottom-div');
+//         task.appendChild(taskBottomDiv);
+
+//         const taskDesc = document.createElement('p');
+//         taskDesc.classList.add('task-desc');
+//         const descInput = document.querySelector('.desc-input');
+//         taskDesc.textContent = descInput.value;
+//         taskBottomDiv.appendChild(taskDesc);
+
+//         const taskDueDate = document.createElement('p');
+//         taskDueDate.classList.add('task-due-date');
+//         const dateInput = document.querySelector('.date-input');
+//         taskDueDate.textContent = dateInput.value;
+//         taskDueDiv.appendChild(taskDueDate);
+
+//         const radioInput = document.querySelector("input[type='radio']:checked");
+//         if (radioInput.value == 'red') {
+//             const taskPriority = document.createElement('div');
+//             taskPriority.classList.add('red-label');
+//             taskDueDiv.appendChild(taskPriority);
+//         } else if (radioInput.value == 'orange') {
+//             const taskPriority = document.createElement('div');
+//             taskPriority.classList.add('orange-label');
+//             taskDueDiv.appendChild(taskPriority);
+//         } else if (radioInput.value == 'yellow') {
+//             const taskPriority = document.createElement('div');
+//             taskPriority.classList.add('yellow-label')
+//             taskDueDiv.appendChild(taskPriority);
+//         }
+
+//         const btnDiv = document.createElement('div');
+//         btnDiv.classList.add('task-btns-div');
+//         taskBottomDiv.appendChild(btnDiv);
+
+//         const editBtn = document.createElement('button');
+//         editBtn.classList.add('edit-task-btn');
+//         editBtn.textContent = 'Edit';
+//         btnDiv.appendChild(editBtn);
+
+//         const deleteBtn = document.createElement('button');
+//         deleteBtn.classList.add('delete-task-btn');
+//         deleteBtn.textContent = 'Delete';
+//         btnDiv.appendChild(deleteBtn);
+
+//         const project = getProject();
+//         console.log(project);
+//         project.addTask(taskInput.value, descInput.value, dateInput.value, radioInput.value);
+
+//         taskInput.value = '';
+//         descInput.value = '';
+//         dateInput.value = '';
+// }
+function createTask(arg) {
     const projectCard = document.querySelector('.project-card');    
         const task = document.createElement('div');
         task.classList.add('task');
@@ -33,7 +109,7 @@ function createTask() {
         const taskTitle = document.createElement('h3');
         taskTitle.classList.add('task-title');
         const taskInput = document.querySelector('.task-input');
-        taskTitle.textContent = taskInput.value;
+
         taskHeader.appendChild(taskTitle);
 
         const taskDueDiv = document.createElement('div');
@@ -47,13 +123,13 @@ function createTask() {
         const taskDesc = document.createElement('p');
         taskDesc.classList.add('task-desc');
         const descInput = document.querySelector('.desc-input');
-        taskDesc.textContent = descInput.value;
+
         taskBottomDiv.appendChild(taskDesc);
 
         const taskDueDate = document.createElement('p');
         taskDueDate.classList.add('task-due-date');
         const dateInput = document.querySelector('.date-input');
-        taskDueDate.textContent = dateInput.value;
+
         taskDueDiv.appendChild(taskDueDate);
 
         const radioInput = document.querySelector("input[type='radio']:checked");
@@ -85,21 +161,32 @@ function createTask() {
         deleteBtn.textContent = 'Delete';
         btnDiv.appendChild(deleteBtn);
 
-        // checkmark creation for when task is finished below
-        // const check = document.createElement('input');
-        // check.classList.add('task-check');
-        // check.type = 'checkbox';
-        // task.appendChild(check);
-
-        const project = getProject();
-        console.log(project);
-        project.addTask(taskInput.value, descInput.value, dateInput.value, radioInput.value);
+        if(arg instanceof Task) {
+            displayTaskValues(taskTitle, taskDesc, taskDueDate, arg)
+        } else {
+            setValues(taskTitle, taskDesc, taskDueDate, radioInput, taskInput, descInput, dateInput)
+        }
 
         taskInput.value = '';
         descInput.value = '';
         dateInput.value = '';
+
 }
 
+function setValues (task, desc, date, radio, taskInput, descInput, dateInput) {
+    const project = getProject();
+    task.textContent = taskInput.value;
+    desc.textContent = descInput.value;
+    date.textContent = dateInput.value;
+    project.addTask(taskInput.value, descInput.value, dateInput.value, radio.value);
+}
+
+function displayTaskValues (title, desc, date, obj){
+    title.textContent = obj.title;
+    desc.textContent = obj.desc;
+    date.textContent = obj.date;
+}
+ 
 function getProject() {
     const mainTitle = document.querySelector('.main-title');
     for(const project of projects){
@@ -109,3 +196,5 @@ function getProject() {
         }  
     }
 }
+
+export {createTask};
--- src/display-project.js
+++ src/display-project.js
@@ -1,8 +1,14 @@
+import { createTask } from "./createTask";
+
 export const displayProject = (proj) => {
     const mainTitle = document.querySelector('.main-title');
-    const submitTaskBtn = document.querySelector('.submit-task');
-    const projectCard = document.querySelector('.project-card');  
+    const projectCard = document.querySelector('.project-card');
     
     mainTitle.textContent = proj.name;
 
+    projectCard.innerHTML = '';
+    for (let i = 0; i < proj.tasks.length; i++) {
+        console.log(proj.tasks[i])
+        createTask(proj.tasks[i]);
+    }
 }
